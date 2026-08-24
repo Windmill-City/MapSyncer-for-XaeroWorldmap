c9b302d0d742e261f728e89bdee8c9995e211500
Windmill_City <1449182174@qq.com>
2026-08-25 01:19:48 +0800

    重构

--- src/main/java/com/mapsyncer/mca/PixelColumnProcessor.java
+++ src/main/java/com/mapsyncer/mca/PixelColumnProcessor.java
@@ -385,7 +385,37 @@ public final class PixelColumnProcessor {
         boolean isGlowing = blockLookup.isGlowing(
                 ChunkParser.getBlockStateAt(stateSection, lx, stateLocalY, lz).name());
 
-        return LightUtil.getLight(
+        return getLight(
                 isSurface, blockLight, skyLight, hasSkyAccess, hasFluidOverlay, isGlowing, worldHasSkylight);
+    }
+
+    static byte getLight(
+            boolean isSurface,
+            byte blockLight,
+            byte skyLight,
+            boolean hasSkyAccess,
+            boolean hasOverlay,
+            boolean isGlowing,
+            boolean worldHasSkylight) {
+
+        if (isGlowing) {
+            return Constants.MAX_LIGHT_LEVEL;
+        }
+
+        if (isSurface) {
+            return blockLight;
+        }
+
+        if (blockLight >= Constants.MAX_LIGHT_LEVEL) {
+            return blockLight;
+        }
+
+        byte effectiveSkyLight = (hasSkyAccess && worldHasSkylight) ? Constants.MAX_LIGHT_LEVEL : skyLight;
+
+        if (!hasOverlay && effectiveSkyLight > blockLight) {
+            return effectiveSkyLight;
+        }
+
+        return blockLight;
     }
 }
